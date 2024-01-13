96c24f9b8b38d2fcaed70e3bf3fcc22931645f38
110306-喻进荣 <yujr@belink.com>
2024-01-13 20:52:51 +0800

    追加虚拟dom作为一个对象动态的传入写死的参数中

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-var myDiv = document.querySelector('div');
-myDiv.innerHTML = "app";
+// let myDiv = document.querySelector('div');
+// myDiv.id = "root";
+// myDiv.innerHTML = "app";
+// let textNode = document.createTextNode('app1');
+// myDiv.append(textNode);
+const vDom = {
+	type: "div",
+	props: {
+		id: "root"
+	},
+	children:{
+		text: "app1"
+	},
+}
+var myDiv = document.querySelector(vDom.type);
+myDiv.id = vDom.props.id;
+let textNode = document.createTextNode(vDom.children.text);
+myDiv.append(textNode);
+
+
+
